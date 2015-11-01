b4e5b849f39386dfc8366b4c0a44c555d5946f16
Quanyails <yijingc@andrew.cmu.edu>
2015-10-31 21:04:48 -0400

    How do I use git? D:

--- CALC.js
+++ CALC.js
@@ -1,12 +1,3 @@
-// http://www.xul.fr/en/html5/filereader.php
-// http://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
-// http://stackoverflow.com/questions/8751020/how-to-get-a-pixels-x-y-coordinate-color-from-an-image
-// http://stackoverflow.com/questions/24148154/image-load-on-end-event
-// http://stackoverflow.com/questions/17714742/looping-through-pixels-in-an-image
-// http://davidwalsh.name/detect-gif-animated
-// imageElement.naturalHeight
-// imageElement.naturalWidth
-
 // Created with raw JavaScript.
 // Libraries would make the code cleaner, but this project is supposed to be small.
 
@@ -62,7 +53,7 @@ var fileInput = document.createElement("input");
 			var file = fileList[0]; // Read only one file
 			fileSize = file.size; // Update global variable
 			fileType = file.type; // Update global variable
-			
+
 			// The reader is created a few lines below.
 			reader.readAsDataURL(fileList[0]); // Implicitly calls onload
 		});
@@ -108,12 +99,12 @@ img.onload = function()
 	};
 
 var loadCanvas = function()
-	{   
+	{
 		// Update the image/canvas.
 		canvas.width = img.width;
 		canvas.height = img.height;
 		canvas.getContext("2d").drawImage(img, 0, 0);
-		
+
 		evaluate();
 	}
 
@@ -121,176 +112,175 @@ var loadCanvas = function()
 var evaluate = function()
 {
 	var width = img.width;
-    var height = img.height;
-	
-    // Returns true if file is in size limits, false if not.
-    var fileSizeCheck = function()
-    {
-        return (fileSize <= maxFileSize);
-    }
-    // Returns true if the file is a PNG, JPEG, or GIF, false if not.
-    // This function does not currently detect whether a GIF is animated.
+	var height = img.height;
+
+	// Returns true if file is in size limits, false if not.
+	var fileSizeCheck = function()
+	{
+		return (fileSize <= maxFileSize);
+	}
+	// Returns true if the file is a PNG, JPEG, or GIF, false if not.
+	// This function does not currently detect whether a GIF is animated.
 	var fileTypeCheck = function()
-    {
+	{
 		var isPNG = fileType === "image/png";
 		var isJPEG = fileType === "image/jpeg";
 		var isGIF = fileType === "image/gif";
-        return (isPNG || isJPEG || isGIF);
-    }
-    
-    // Returns true if both dimensions are in the ranges, false if at least one
+		return (isPNG || isJPEG || isGIF);
+	}
+
+	// Returns true if both dimensions are in the ranges, false if at least one
 	// of them is not.
-    var dimensionsCheck = function()
-    {
-        var widthInRange = minDimension <= width && width <= maxDimension;
-        var heightInRange = minDimension <= height && height <= maxDimension;
-        return widthInRange && heightInRange;
-    }
-    
-    // TestFunction must take in an array of four numbers and returns a bool.
-    // Threshold represents the minimum percentage of pixels that must pass the
-    // test for the entire test to be considered 'passing'/'true'.
-    var testCanvasSelection = function(x, y, w, h, pixelTester, threshold)
-    {
-        var selection = canvas.getContext("2d")
-        	.getImageData(0, 0, w, h);
+	var dimensionsCheck = function()
+	{
+		var widthInRange = minDimension <= width && width <= maxDimension;
+		var heightInRange = minDimension <= height && height <= maxDimension;
+		return widthInRange && heightInRange;
+	}
 
-        var pixelsToArray = function(imageData)
-        {
-            var pixelArray = imageData.data;
-            var pixelLength = pixelArray.length >> 2; // >> 2 to avoid floats?
-            var arr = new Array(pixelLength);
-            for (var i = 0; i < pixelLength; i++)
-            {arr[i] = new Array(4);} // 4 elements for RGBA values
+	// TestFunction must take in an array of four numbers and returns a bool.
+	// Threshold represents the minimum percentage of pixels that must pass the
+	// test for the entire test to be considered 'passing'/'true'.
+	var testCanvasSelection = function(x, y, w, h, pixelTester, threshold)
+	{
+		var selection = canvas.getContext("2d").getImageData(0, 0, w, h);
 
-            pixelArray.forEach(function(d, i)
-                               {
-                var index = i >> 2;
-                var channel = i % 4;
-                arr[index][channel] = d;
-            })
-            return arr;
-        }
-        var pixels = pixelsToArray(selection);
-        var passedPixels = pixels.filter(pixelTester);
+		var pixelsToArray = function(imageData)
+		{
+			var pixelArray = imageData.data;
+			var pixelLength = pixelArray.length >> 2; // >> 2 to avoid floats?
+			var arr = new Array(pixelLength);
+			for (var i = 0; i < pixelLength; i++)
+			{arr[i] = new Array(4);} // 4 elements for RGBA values
 
-        return (passedPixels.length / pixels.length >= threshold);
-    }
-    
-   	// Check border pixels for white/transparency.
-    // Returns true if the background is suitable, false if not.
-    var backgroundCheck = function()
-    {
-        // TODO: More precise thresholds?
-        var threshold = 0.8;
-        // Minimum channel value needed for a color to be considered 'white'.
-        var limit = 240;
-        
-        var isBackground = function(pixel)
-        {
-            var r = pixel[0];
-            var g = pixel[1];
-            var b = pixel[2];
-            var a = pixel[3];
+			pixelArray.forEach(function(d, i)
+							   {
+				var index = i >> 2;
+				var channel = i % 4;
+				arr[index][channel] = d;
+			})
+			return arr;
+		}
+		var pixels = pixelsToArray(selection);
+		var passedPixels = pixels.filter(pixelTester);
 
-            var isWhite = (r - limit > 0) && (g - limit > 0) && (b - limit > 0);
-            var isTransparent = (a !== 255);
+		return (passedPixels.length / pixels.length >= threshold);
+	}
 
-            return isWhite && !isTransparent;
-        }
-        
-        var left = testCanvasSelection(0, 0, 1, height, isBackground, threshold);
-        var right = testCanvasSelection(width - 1, 0, 1, height, isBackground, threshold);
-        var top = testCanvasSelection(0, 0, width, 1, isBackground, threshold);
-        var bottom = testCanvasSelection(0, height - 1, width, 1, isBackground, threshold);
-        
-        return left && right && top && bottom;
-    }
-    
-    var clipCheck = function()
-    {
+	// Check border pixels for white/transparency.
+	// Returns true if the background is suitable, false if not.
+	var backgroundCheck = function()
+	{
 		// TODO: More precise thresholds?
-        var threshold = 0.95;
-        // Minimum channel value needed for a color to be considered 'white'.
-        var limit = 240;
-        
-        var doesNotClip = function(pixel)
-        {
-            var r = pixel[0];
-            var g = pixel[1];
-            var b = pixel[2];
-            var a = pixel[3];
+		var threshold = 0.8;
+		// Minimum channel value needed for a color to be considered 'white'.
+		var limit = 240;
 
-            var noClip = (r - limit > 0) && (g - limit > 0) && (b - limit > 0);
+		var isBackground = function(pixel)
+		{
+			var r = pixel[0];
+			var g = pixel[1];
+			var b = pixel[2];
+			var a = pixel[3];
 
-            return noClip;
-        }
-        
-        var left = testCanvasSelection(0, 0, 1, height, doesNotClip, threshold);
-        var right = testCanvasSelection(width - 1, 0, 1, height, doesNotClip, threshold);
-        var top = testCanvasSelection(0, 0, width, 1, doesNotClip, threshold);
-        var bottom = testCanvasSelection(0, height - 1, width, 1, doesNotClip, threshold);
-        
-        return left && right && top && bottom;
-    }
-    
+			var isWhite = (r - limit > 0) && (g - limit > 0) && (b - limit > 0);
+			var isTransparent = (a !== 255);
+
+			return isWhite && !isTransparent;
+		}
+
+		var left = testCanvasSelection(0, 0, 1, height, isBackground, threshold);
+		var right = testCanvasSelection(width - 1, 0, 1, height, isBackground, threshold);
+		var top = testCanvasSelection(0, 0, width, 1, isBackground, threshold);
+		var bottom = testCanvasSelection(0, height - 1, width, 1, isBackground, threshold);
+
+		return left && right && top && bottom;
+	}
+
+	var clipCheck = function()
+	{
+		// TODO: More precise thresholds?
+		var threshold = 0.95;
+		// Minimum channel value needed for a color to be considered 'white'.
+		var limit = 240;
+
+		var doesNotClip = function(pixel)
+		{
+			var r = pixel[0];
+			var g = pixel[1];
+			var b = pixel[2];
+			var a = pixel[3];
+
+			var noClip = (r - limit > 0) && (g - limit > 0) && (b - limit > 0);
+
+			return noClip;
+		}
+
+		var left = testCanvasSelection(0, 0, 1, height, doesNotClip, threshold);
+		var right = testCanvasSelection(width - 1, 0, 1, height, doesNotClip, threshold);
+		var top = testCanvasSelection(0, 0, width, 1, doesNotClip, threshold);
+		var bottom = testCanvasSelection(0, height - 1, width, 1, doesNotClip, threshold);
+
+		return left && right && top && bottom;
+	}
+
 	// Run each check and save the results.
-    
-    var fsResult = fileSizeCheck();
-    var ftResult = fileTypeCheck();
-    var dResult = dimensionsCheck();
+	
+	var fsResult = fileSizeCheck();
+	var ftResult = fileTypeCheck();
+	var dResult = dimensionsCheck();
 	var bResult = backgroundCheck();
-    var cResult = clipCheck();
-    
+	var cResult = clipCheck();
+
 	// Evaluate result of each check and adds its corresponding issue, if any.
-    var issues = [];
-    if (!fsResult)
-    {
-        issues.push("The image uploaded exceeds the limit of " + maxFileSize + " bytes.");
-    }
-    if (!ftResult)
-    {
-        issues.push("The file is not of the JPEG, PNG, or GIF image types.");
-    }
-    if (!dResult)
-    {
-        issues.push("The image is not between " + minDimension + " and " + maxDimension + " in dimension.");
-    }
-    if (!bResult)
-    {
-        issues.push("The background is not solid, non-transparent white.");
-    }
+	var issues = [];
+	if (!fsResult)
+	{
+		issues.push("The image uploaded exceeds the limit of " + maxFileSize + " bytes.");
+	}
+	if (!ftResult)
+	{
+		issues.push("The file is not of the JPEG, PNG, or GIF image types.");
+	}
+	if (!dResult)
+	{
+		issues.push("The image is not between " + minDimension + " and " + maxDimension + " in dimension.");
+	}
+	if (!bResult)
+	{
+		issues.push("The background is not solid, non-transparent white.");
+	}
 	if (!cResult)
-    {
-        issues.push("The image clips off the image's canvas.");
-    }
-    
-    // If we had no issues, report that there are no issues.
-    if (issues.length === 0)
-    {
-        issues.push("There are no technical issues detected with the image!");
-    }
-    
-    report(issues);
+	{
+		issues.push("The image clips off the image's canvas.");
+	}
+
+	// If we had no issues, report that there are no issues.
+	if (issues.length === 0)
+	{
+		issues.push("There are no technical issues detected with the image!");
+	}
+
+	report(issues);
 }
 
 var report = function(iarr)
 {
-    // Clear existing list of issues.
-    var c = outputList.firstChild;
-    while (c != null)
-    {
-        outputList.removeChild(c);
-        c = outputList.firstChild;
-    }
-    
+	// Clear existing list of issues.
+	var c = outputList.firstChild;
+	while (c != null)
+	{
+		outputList.removeChild(c);
+		c = outputList.firstChild;
+	}
+
 	// List the new issues in an HTML list.
-    iarr.forEach(function(str)
+	iarr.forEach(function(str)
 		{
-        	var li = document.createElement("li");
+			var li = document.createElement("li");
 				li.innerHTML = str;
 				outputList.appendChild(li);
-    	});
+		});
 }
 
 })()
