4775b479deaadb1ba9fc80ae59c1dc3d21deb630
Yijing Chen <yijing@duolingo.com>
2021-12-28 19:46:59 -0500

    Install react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-const App = () => <div>Hello, world!</div>;
+import { HashRouter } from "react-router-dom";
+
+const App = () => (
+  <HashRouter>
+    <div>Hello, world!</div>
+  </HashRouter>
+);
 
 export default App;
